d56f8767b80410dd92149d490b39465db5be5725
pabejara <pablobejarano@gmail.com>
2022-11-26 22:52:48 -0500

    second_vertion

--- src/App.jsx
+++ src/App.jsx
@@ -10,40 +10,39 @@ function App() {
   const [weather, setWeather] = useState()
   const [temp, setTemp] = useState()
 
-const success = pos => {
-  setCoords({
-    lat: pos.coords.latitude,
-    lon: pos.coords.longitude
-  })
-}
+  const success = pos => {
+    setCoords({
+      lat: pos.coords.latitude,
+      lon: pos.coords.longitude
+    })
+  }
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(success)
   }, [])
-  
+
   useEffect(() => {
-    if(coords){
+    if (coords) {
       const apiKey = `53289a293dd2535b8918dbb007a475ce`
       const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${apiKey}`
       axios.get(URL)
         .then(res => {
           setWeather(res.data)
           const celsius = (res.data.main.temp - 273.15).toFixed(1)
-          const farenheit = (celsius * (9/5) + 32).toFixed(1)
-          setTemp({celsius, farenheit})
+          const farenheit = (celsius * (9 / 5) + 32).toFixed(1)
+          setTemp({ celsius, farenheit })
         })
         .catch(err => console.error(err))
     }
   }, [coords])
 
 
-
   return (
     <div className="App">
-      <WeatherCard 
-      weather={weather}
-      temp = {temp}
-      /> 
+      <WeatherCard
+        weather={weather}
+        temp={temp}
+      />
     </div>
   )
 }
--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -9,34 +9,34 @@ const WeatherCard = ({ weather, temp }) => {
 
     return (
         <article className='card' >
-            <header>
+            <header className='card__header' >
                 <h1 className='card__title' >Wheather App</h1>
                 <h2 className='card__subtitle' >{weather?.name}, {weather?.sys.country}</h2>
-                <section className='card__icon-container' >
-                    <img className='card__icon' src={weather && `http://openweathermap.org/img/wn/${weather?.weather[0].icon}@4x.png`} alt="" />
-                <h3 className='card__temp' >{weather?.main.temp}</h3>
-                </section>
-                <section className='card__info'  >
-                    <h3>"{weather?.weather[0].description}"</h3>
-                    <ul className='list' >
-                        <li className='card__item'> <span>Wind Speed</span> {weather?.wind.speed} m/s </li>
-                        <li className='card__item'> <span> clouds</span> {weather?.clouds.all} %</li>
-                        <li className='card__item'> <span>Pressure </span> {weather?.main.pressure} hPa</li>
-                    </ul>
-                </section>
             </header>
-            <h3 className="card__temp">
-                        {
-                            isCelsius ?
-                                `${temp?.celsius} ºC`
-                                :
-                                `${temp?.farenheit} ºF`
-                        }
-                    </h3>
-            <footer>
+            <section className='card__icon-container' >
+                <img className='card__icon' src={weather && `http://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`} alt="" />
+
+                <h3 className="card__temp">
+                    {
+                        isCelsius ?
+                            `${temp?.celsius} ºC`
+                            :
+                            `${temp?.farenheit} ºF`
+                    }
+                </h3>
+            </section>
+
+            <section className='card__info'  >
+                <h3 className='card__description' >"{weather?.weather[0].description}"</h3>
+                <ul className='list' >
+                    <li className='card__item'> <span>Wind Speed</span> {weather?.wind.speed} m/s </li>
+                    <li className='card__item'> <span> clouds</span> {weather?.clouds.all} %</li>
+                    <li className='card__item'> <span>Pressure </span> {weather?.main.pressure} hPa</li>
+                </ul>
+            </section>
+            <footer className='card__footer' >
                 <button onClick={handleClick} className="card__btn">Change to {isCelsius ? 'ºF' : 'ºC'}</button>
             </footer>
-
         </article>
     )
 }
